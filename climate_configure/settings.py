645c5473fad093ba0e4ddd3b7cc1bca02f130d8e
IsmaelKiprop <kipropismael27@gmail.com>
2023-10-27 21:43:00 +0300

    resetpassword

--- climate_configure/settings.py
+++ climate_configure/settings.py
@@ -9,14 +9,14 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Define the default auto field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
