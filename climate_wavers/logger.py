99bafddc6d754924a82f31fb9edbf7c92867b74a
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2023-11-08 15:51:01 +0300

    Update logger.py

--- climate_wavers/logger.py
+++ climate_wavers/logger.py
@@ -5,7 +5,3 @@ logging.basicConfig(filename='climatewavers.log', level=logging.INFO, format='%(
 
 # Create a logger instance
 logger = logging.getLogger('climatewavers')
-<<<<<<< HEAD
-=======
-
->>>>>>> 4140ab6d15dae9123fb58043af9b4cd6d7c2c247
