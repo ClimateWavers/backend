4f1db39f6bb0692610227ef72d32348bcc359acc
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-11-13 02:54:40 +0100

    deployment automated

--- automate_deployment/build.sh
+++ automate_deployment/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker login quay.io
-docker build -t quay.io/olagoldhackxx/climatewavers-backend:v1 .
-docker push quay.io/olagoldhackxx/climatewavers-backend:v1
+docker build -t quay.io/olagoldhackxx/backend:v1 .
+docker push quay.io/olagoldhackxx/backend:v1
+
